4276db106482b4c336c6c57071e49b6ae4e2b7e5
Shah Isha Redohy <blackeyes0043@gmail.com>
2023-10-08 00:18:26 -0400

    Removed cart.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { CartIcon } from './components/cartIcon';
 import Cars from './pages/Cars';
-import Cart from './pages/Cart';
 import Car from './pages/Car';
 
 const App = () => {
   return (
     <div className="App">
       <Router>
-        <CartIcon/>
         <Routes>
           <Route path='/' exact element={<Cars/>} />
           <Route path='/:car_id' element={<Car/>}/>
